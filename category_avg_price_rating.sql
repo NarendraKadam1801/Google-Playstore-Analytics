be94dde04b73f71b7b3d6f9e6183824f02b730f4
Narendra Kadam <161104002+NarendraKadam1801@users.noreply.github.com>
2025-09-20 20:26:04 +0530

    Delete category_avg_price_rating.sql

--- category_avg_price_rating.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    c.CategoryName,
-    AVG(a.Price) AS AvgPrice,
-    AVG(a.Rating) AS AvgRating,
-    COUNT(*) AS AppCount
-FROM apps a
-JOIN categories c ON a.CategoryID = c.CategoryID
-GROUP BY c.CategoryName
-ORDER BY AvgRating DESC;
